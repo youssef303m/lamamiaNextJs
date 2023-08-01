c08215dd89fe591e98371330ccbd4032155fa53f
Youssef303M <youssef303m@gmail.com>
2023-08-02 01:41:05 +0300

    Styling (Global and Module CSS)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import React from "react";
+import styles from "./navbar.module.css";
 
 const links = [
   {
@@ -38,15 +39,18 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">lamamia</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href="" className={styles.logo}>
+        lamamia
+      </Link>
+      <div className={styles.links}>
         {links.map((link) => (
-          <Link key={link.id} href={link.url}>
+          <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
         <button
+          className={styles.logout}
           onClick={() => {
             console.log("logged out");
           }}
